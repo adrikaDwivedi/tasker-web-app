25e1195fd982157f2270541b954bdde73ba5f80f
adrikaD <adrikad97@gmail.com>
2026-05-19 21:12:31 +0530

    first commit splashscreen with side and taskbar

---
+++ components/layout/Sidebar.jsx
@@ -0,0 +1,29 @@
+import {
+  LayoutDashboard,
+  CheckSquare,
+  BarChart3,
+  Trophy,
+  Calendar,
+  Target,
+  Settings,
+} from "lucide-react";
+
+function Sidebar(){
+    return (
+        <>
+        <header>
+
+        </header>
+
+        <main>
+
+        </main>
+
+        <footer>
+
+        </footer>
+        </>
+    )
+}
+
+export default Sidebar;
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import SplashScreen from '../components/layout/SplashScreen'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+  <SplashScreen/>
+    </>
+  )
+}
+
+export default App
